68a517d684c20037b0040c48d760b3001e849acc
MadaraUchiha-314 <rohithr31@gmail.com>
2019-01-24 22:24:32 +0530

    Addng the install instructions or wnidows

--- src/install-dependencies/install-dependencies-win.bat
+++ src/install-dependencies/install-dependencies-win.bat
@@ -1,3 +1,4 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install python chromedriver winflexbison mingw
+cinst winflexbison make mingw python
 pip install selenium requests pysocks urllib3 --upgrade
+rereshenv

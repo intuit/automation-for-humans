fc3fd7d97efabae4953ce4cf1203db3509789c65
MadaraUchiha-314 <rohithr31@gmail.com>
2019-01-28 15:06:30 +0530

    Adding installation instructions

--- src/install-dependencies/install-dependencies-mac.sh
+++ src/install-dependencies/install-dependencies-mac.sh
@@ -2,6 +2,7 @@
 brew update
 brew install python || true
 brew install brew-cask || true
-brew cask install google-chrome chromedriver || true
+brew cask install google-chrome || true
+brew cask install chromedriver || true
 brew install flex bison || true
-pip install selenium pysocks urllib3 requests
+pip install selenium pysocks urllib3 requests pyperclip

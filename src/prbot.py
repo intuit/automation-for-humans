18ef9ad7310ce49d9bc25a90f39ad271f62d4e0e
MadaraUchiha-314 <rohithr31@gmail.com>
2019-03-25 14:47:47 +0530

    Adding the api url override for PRBOT

--- src/prbot.py
+++ src/prbot.py
@@ -7,8 +7,7 @@ from constants import *
 
 # This function is generic but only works with CircleCI :p
 # TODO: Make this generic so that it works with all the 3 CI enviornments.
-def comment_on_pr(body) :
-    GITHUB_API_ENDPOINT = "https://api.github.com"
+def comment_on_pr(body, api_end_point = "https://api.github.com") :
     AUTH_HEADER = {}
     AUTH_HEADER["Authorization"] = "token " + os.environ["GITHUB_PERSONAL_TOKEN"]
 
@@ -32,7 +31,15 @@ def comment_on_pr(body) :
     print(r)
 
 if __name__ == "__main__" :
-    # Open's the performance file and comment on the PR
-    with open(PERFORMANCE_REPORT, "r") as perf_report_file :
-        body = perf_report_file.read()
-        comment_on_pr(body)
+    try :
+        # Open's the performance file and comment on the PR
+        with open(PERFORMANCE_REPORT, "r") as perf_report_file :
+            body = perf_report_file.read()
+
+            # The first argument can be used to override
+            if len(sys.argv) > 1 :
+                comment_on_pr(body, sys.argv[1])
+            else :
+                comment_on_pr(body)
+    except Exception :
+        print("[LOG] Could not find performance file. Exiting.")
